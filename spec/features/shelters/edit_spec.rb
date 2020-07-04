995f31605510736506629998bfee685293709ef2
Norma Lopez <norma_l304@yahoo.com>
2020-07-03 22:07:33 -0600

    Test edit shelter info

---
+++ spec/features/shelters/edit_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'edit shelter' do
+  it 'can edit an individual shelter info' do
+    shelter_1 = Shelter.create(name: "North Denver Pet Shel",
+                               address: "12345 North Rd",
+                               city: "Denver",
+                               state: "Colorado",
+                               zip: 80002)
+    visit '/shelters'
+
+    expect(page).to_not have_content("North Denver Pet Shelter")
+
+    click_on "Update Shelter"
+
+    expect(current_path).to eq("/shelters/#{shelter_1.id}/edit")
+
+    fill_in :name, with: "North Denver Pet Shelter"
+
+    click_on "Update Shelter"
+
+    expect(current_path).to eq('/shelters')
+    expect(page).to have_content("North Denver Pet Shelter")
+  end
+end
