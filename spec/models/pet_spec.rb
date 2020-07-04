1a82693905634ac6c7e668c6ad5f64fd7837c186
Norma Lopez <norma_l304@yahoo.com>
2020-07-04 16:45:48 -0600

    Test validation for pet info

---
+++ spec/models/pet_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Pet do
+  describe 'validations' do
+    it { should validate_presence_of :image}
+    it { should validate_presence_of :name}
+    it { should validate_presence_of :description}
+    it { should validate_presence_of :approximate_age}
+    it { should validate_presence_of :sex}
+    it { should validate_presence_of :adoption_status}
+  end
+end
