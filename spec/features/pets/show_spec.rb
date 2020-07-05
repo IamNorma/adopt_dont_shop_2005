d558cdf67fe37201dc7f3044ac51aba1fd115bb9
Norma Lopez <norma_l304@yahoo.com>
2020-07-05 15:45:43 -0600

    Add shelter_1 and shelter_id to pet_1

--- spec/features/pets/show_spec.rb
+++ spec/features/pets/show_spec.rb
@@ -2,12 +2,18 @@ require 'rails_helper'
 
 RSpec.describe "page for individual pet" do
   it 'can display information for an individual pet' do
+    shelter_1 = Shelter.create!(name: "Southeast Denver Pet Shelter",
+                               address: "12345 S Parker Rd",
+                               city: "Denver",
+                               state: "Colorado",
+                               zip: 80231)
     pet_1 = Pet.create!(image: "https://www.teacupdogdaily.com/wp-content/uploads/2016/05/Chihuahua-Splashed-markings.jpg",
                        name: "Tito",
                        description: "Tito loves kids and playing fetch with empty plastic water bottles.",
                        approximate_age: "5 years",
                        sex: "Male",
-                       adoption_status: "Pending")
+                       adoption_status: "Pending",
+                       shelter_id: shelter_1.id)
 
     visit "/pets/#{pet_1.id}"
 
