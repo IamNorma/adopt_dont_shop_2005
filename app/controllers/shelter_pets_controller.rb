571faf865191ea810a32f608367e76510ad82ce6
Norma Lopez <norma_l304@yahoo.com>
2020-07-05 23:47:55 -0600

    Add root route

--- app/controllers/shelter_pets_controller.rb
+++ app/controllers/shelter_pets_controller.rb
@@ -5,8 +5,21 @@ class ShelterPetsController < ApplicationController
    @adoptable_pets = adoptable
  end
 
+ def new
+ end
+
+ def create
+   shelter = Shelter.find(params[:id])
+   pet = shelter.pets.create(pet_params)
+   redirect_to "/shelters/#{shelter.id}/pets"
+ end
+
  private
  def adoptable
    @pets.where(adoption_status: "Adoptable")
+ end
+
+ def pet_params
+   params.permit(:image, :name, :description, :approximate_age, :sex, :adoption_status)
  end
 end
